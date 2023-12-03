910937944a2acf0743f34855055f5fd12e28d77b
joksulainen <54633559+joksulainen@users.noreply.github.com>
2023-12-04 01:06:21 +0200

    i dont think this works, needs to be tested anyway

--- helpers/permission_decorators.py
+++ helpers/permission_decorators.py
@@ -5,14 +5,14 @@ from helpers.json_wrappers import CONFIG
 
 def is_admin():
     def predicate(ctx: Context) -> bool:
-        if not ctx.author.id in CONFIG.admin_ids:
+        if not ctx.author.id in CONFIG.admin_ids and not (ctx.author.id == ctx.bot.owner_id or ctx.author.id in ctx.bot.owner_ids):
             raise NotAdmin()
         return True
     return check(predicate)
 
 def is_moderator():
     def predicate(ctx: Context) -> bool:
-        if not ctx.author.id in CONFIG.moderator_ids + CONFIG.admin_ids:
+        if not (ctx.author.id in CONFIG.moderator_ids + CONFIG.admin_ids) and not (ctx.author.id == ctx.bot.owner_id or ctx.author.id in ctx.bot.owner_ids):
             raise NotModerator()
         return True
     return check(predicate)
