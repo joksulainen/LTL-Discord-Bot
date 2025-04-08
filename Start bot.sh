218b3e390fa95a9dd6b144f219b050cc0e734b59
joksulainen <54633559+joksulainen@users.noreply.github.com>
2025-04-08 16:43:53 +0300

    figured out the path to the venv python on linux

--- Start bot.sh
+++ Start bot.sh
@@ -1,2 +1,2 @@
-# will try to get this to dyna mically use uv if its installed at some point
-./.venv/Scripts/python ./src/main.py
+# will try to get this to dynamically use uv if its installed at some point
+./.venv/bin/python ./src/main.py
