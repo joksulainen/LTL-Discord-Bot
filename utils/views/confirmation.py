0568aa8d7d8d3ee0ae0535c6c2cbd978a8fdfcdd
joksulainen <54633559+joksulainen@users.noreply.github.com>
2025-04-08 11:28:55 +0300

    change some params

--- utils/views/confirmation.py
+++ utils/views/confirmation.py
@@ -6,7 +6,7 @@ class ConfirmationView(View):
     """View for actions that require confirmation. `decision` is a bool or None if there is no response or the view timed out."""
     decision: bool | None = None
     
-    def __init__(self, /, timeout: float = 30, disable_on_timeout: bool = True, **kwargs):
-        super().__init__(timeout=timeout, disable_on_timeout=disable_on_timeout, **kwargs)
+    def __init__(self, *, timeout: float = 30, **kwargs):
+        super().__init__(timeout=timeout, disable_on_timeout=True, **kwargs)
         self.add_item(Button(label="Yes", style=discord.ButtonStyle.green))
         self.add_item(Button(label="No", style=discord.ButtonStyle.red))
