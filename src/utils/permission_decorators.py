9cd146318d9f23a88c136e7ea9bb8356b67af9b5
joksulainen <54633559+joksulainen@users.noreply.github.com>
2025-04-11 00:24:30 +0300

    transform is_admin logic to be a more readable equivalent

--- src/utils/permission_decorators.py
+++ src/utils/permission_decorators.py
@@ -4,8 +4,7 @@ from discord.ext.commands import CheckFailure, check
 
 def is_admin():
     async def predicate(ctx: ApplicationContext) -> bool:
-        if (not ctx.author.id in ctx.bot.config.admin_ids and
-            not await ctx.bot.is_owner(ctx.author)):
+        if not (ctx.author.id in ctx.bot.config.admin_ids or await ctx.bot.is_owner(ctx.author)):
             raise NotAdmin()
         return True
     return check(predicate)
