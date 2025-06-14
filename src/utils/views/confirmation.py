29d71526a7d261daef83f1cf7e38bd8394373665
joksulainen <54633559+joksulainen@users.noreply.github.com>
2025-06-14 23:41:36 +0300

    change confirmation view to have label parameters

--- src/utils/views/confirmation.py
+++ src/utils/views/confirmation.py
@@ -6,7 +6,13 @@ class ConfirmationView(View):
     """View for actions that require confirmation. `decision` is a bool or None if there is no response or the view timed out."""
     decision: bool | None = None
     
-    def __init__(self, *, timeout: float = 30, **kwargs):
-        super().__init__(timeout=timeout, disable_on_timeout=True, **kwargs)
-        self.add_item(Button(label="Yes", style=discord.ButtonStyle.green))
-        self.add_item(Button(label="No", style=discord.ButtonStyle.red))
+    def __init__(self, *,
+            timeout: float = 30,
+            disable_on_timeout: bool = True,
+            lbl_yes: str = "Yes",
+            lbl_no: str = "No",
+            **kwargs
+        ):
+        super().__init__(timeout=timeout, disable_on_timeout=disable_on_timeout, **kwargs)
+        self.add_item(Button(label=lbl_yes, style=discord.ButtonStyle.green))
+        self.add_item(Button(label=lbl_no, style=discord.ButtonStyle.red))
