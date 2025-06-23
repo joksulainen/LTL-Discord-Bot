5f7f0314ce07c0dd0a512f5e0aca9699b5af80d1
joksulainen <54633559+joksulainen@users.noreply.github.com>
2025-06-23 03:14:38 +0300

    let there be leaderboard formatting

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,3 +1,3 @@
 from . import permission_decorators, leaderboard
 from .permission_decorators import is_admin
-from .leaderboard import LeaderboardEntry
+from .leaderboard import LeaderboardEntry, format_lb_entry, strftdelta
--- src/utils/leaderboard.py
+++ src/utils/leaderboard.py
@@ -1,3 +1,4 @@
+import datetime
 from dataclasses import dataclass
 
 
@@ -5,3 +6,28 @@ from dataclasses import dataclass
 class LeaderboardEntry:
     user_id: int
     eliminated_at: float
+
+
+def strftdelta(tdelta: datetime.timedelta) -> str:
+    # get the individual time components
+    total = tdelta.total_seconds()
+    days = total // 86400
+    hours, rem = divmod(total % 86400, 3600)
+    minutes, seconds = divmod(rem, 60)
+    
+    # build the final string
+    # this is ugly but it does what i need it to do
+    string = ""
+    if days > 0:
+        string += f"{int(days)}d {int(hours)}h {int(minutes)}m "
+    elif hours > 0:
+        string += f"{int(hours)}h {int(minutes)}m "
+    elif minutes > 0:
+        string += f"{int(minutes)}m "
+    string += f"{int(seconds)}s"
+    
+    return string
+
+def format_lb_entry(entry: LeaderboardEntry, start_time: float) -> str:
+    delta = datetime.datetime.fromtimestamp(entry.eliminated_at, datetime.UTC) - datetime.datetime.fromtimestamp(start_time, datetime.UTC)
+    return f"<@{entry.user_id}> - `{strftdelta(delta)}`"
