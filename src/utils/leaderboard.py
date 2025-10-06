c5bf015ea0986c60324bac9feca6883960fe1fe5
joksulainen <54633559+joksulainen@users.noreply.github.com>
2025-10-06 20:08:21 +0300

    remove days from duration

--- src/utils/leaderboard.py
+++ src/utils/leaderboard.py
@@ -11,16 +11,13 @@ class LeaderboardEntry:
 def strftdelta(tdelta: datetime.timedelta) -> str:
     # get the individual time components
     total = tdelta.total_seconds()
-    days = total // 86400
-    hours, rem = divmod(total % 86400, 3600)
-    minutes, seconds = divmod(rem, 60)
+    hours = total // 3600
+    minutes, seconds = divmod(hours % 3600, 60)
     
     # build the final string
     # this is ugly but it does what i need it to do
     string = ""
-    if days > 0:
-        string += f"{int(days)}d {int(hours)}h {int(minutes)}m "
-    elif hours > 0:
+    if hours > 0:
         string += f"{int(hours)}h {int(minutes)}m "
     elif minutes > 0:
         string += f"{int(minutes)}m "
